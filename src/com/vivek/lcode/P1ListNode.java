c9beabd6d84a1516c9846dc9597e11045fe57ed1
vivek <vivek.dubeydeveloper@gmail.com>
2022-05-15 14:32:19 +0530

    Problem1

---
+++ src/com/vivek/lcode/P1ListNode.java
@@ -0,0 +1,8 @@
+package com.vivek.lcode;
+public class P1ListNode {
+      int val;
+      P1ListNode next;
+      P1ListNode() {}
+      P1ListNode(int val) { this.val = val; }
+      P1ListNode(int val, P1ListNode next) { this.val = val; this.next = next; }
+}
---
+++ src/com/vivek/lcode/P1Solution.java
@@ -0,0 +1,13 @@
+package com.vivek.lcode;
+
+public class P1Solution {
+    public P1ListNode mergeTwoLists(P1ListNode list1, P1ListNode list2) {
+        
+    	return null;
+    }
+}
+
+
+
+
+ 
